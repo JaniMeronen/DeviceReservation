d09a7e9503e1ca9c9af2e28cc1252f266a317a79
Jani Meronen <jani.meronen@eng.tamk.fi>
2019-05-22 20:57:19 +0300

    Apply migrations at startup

--- DeviceReservation/Program.cs
+++ DeviceReservation/Program.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore;
+﻿using DeviceReservation.Models;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DeviceReservation
 {
     class Program
     {
-        static void Main(string[] args) =>
-            CreateWebHostBuilder(args).Build().Run();
+        static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            var context = host.Services.GetService<DataContext>();
+            if (context.Database.IsSqlServer())
+            {
+                context.Database.Migrate();
+            }
+
+            host.Run();
+        }
 
         static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
